0633fb92e333b99843d152c77d5ec51a5c0b7fe7
AnimationGamerStudios <101293629+AnimationGamerStudios@users.noreply.github.com>
2022-08-20 19:40:47 -0400

    Copium Crystal Item

---
+++ src/main/java/net/mcreator/life_statues/item/CopiumCrystalItem.java
@@ -0,0 +1,44 @@
+
+package net.mcreator.life_statues.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.life_statues.LifeStatuesModElements;
+
+@LifeStatuesModElements.ModElement.Tag
+public class CopiumCrystalItem extends LifeStatuesModElements.ModElement {
+	@ObjectHolder("life_statues:copium_crystal")
+	public static final Item block = null;
+
+	public CopiumCrystalItem(LifeStatuesModElements instance) {
+		super(instance, 39);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("copium_crystal");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
