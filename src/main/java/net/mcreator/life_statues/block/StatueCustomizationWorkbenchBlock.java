fadd5a61a83238715f886e8817cf3a90c1b960f3
AnimationGamerStudios <101293629+AnimationGamerStudios@users.noreply.github.com>
2022-08-29 11:06:30 -0400

    Customization Table

---
+++ src/main/java/net/mcreator/life_statues/block/StatueCustomizationWorkbenchBlock.java
@@ -0,0 +1,58 @@
+
+package net.mcreator.life_statues.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.life_statues.itemgroup.LifeStatuesTabItemGroup;
+import net.mcreator.life_statues.LifeStatuesModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@LifeStatuesModElements.ModElement.Tag
+public class StatueCustomizationWorkbenchBlock extends LifeStatuesModElements.ModElement {
+	@ObjectHolder("life_statues:statue_customization_workbench")
+	public static final Block block = null;
+
+	public StatueCustomizationWorkbenchBlock(LifeStatuesModElements instance) {
+		super(instance, 46);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(LifeStatuesTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("statue_customization_workbench");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
